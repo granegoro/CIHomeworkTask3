2b758384be0d88c3f609b33de540bb962d775d2b
Егор Старшелюк <granegoro@gmail.com>
2022-09-28 20:10:54 +0300

    EchoTest expected to pass

--- src/test/java/EchoTest.java
+++ src/test/java/EchoTest.java
@@ -17,7 +17,7 @@ class EchoTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some value"))
         ;
     }
 
