a788b8ccc48760f12532a7362c53f55f579892fc
Егор Старшелюк <granegoro@gmail.com>
2022-09-28 20:03:34 +0300

    Initial commit

---
+++ src/test/java/EchoTest.java
@@ -0,0 +1,24 @@
+import groovy.transform.ASTTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class EchoTest {
+    @Test
+    void shouldReturnPostQuery() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some value")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+        ;
+    }
+
+}
