75bed2962bea5ae1437dab46c9a8a75bd45d452e
Егор Старшелюк <granegoro@gmail.com>
2022-09-28 20:08:48 +0300

    EchoTest error expected

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/27stjxl39y5uo1e2?svg=true)](https://ci.appveyor.com/project/granegoro/cihomeworktask3)
--- src/test/java/EchoTest.java
+++ src/test/java/EchoTest.java
@@ -5,6 +5,21 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 class EchoTest {
+    @Test
+    void shouldReturnPostQuery() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some value")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
 
 
 }
